ecf9364fa4e599e6b40051f0451967d7763ab415
KennethMinn <minthukha2472005@gmail.com>
2024-04-21 19:40:12 +0630

    Stream and buffer

---
+++ stream&buffer/index.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+//read and write file with buffer - chunk by chunk
+const readStream = fs.createReadStream("./docs/large.txt");
+const writeStream = fs.createWriteStream("./docs/large_write.txt");
+
+//pipe
+// readStream.on("data", (data) => {
+//   console.log(data.toString()); //convert buffer to string
+//   console.log("__chunk__");
+//   writeStream.write(data.toString()); //write large file
+// });
+
+//pipe - short way
+readStream.pipe(writeStream); //instead of writing the whole large file, we write it by chunk - stream&buffer
