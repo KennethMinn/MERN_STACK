67950d9556a4e7fd228fb6773a41a38539dd84a5
KennethMinn <minthukha2472005@gmail.com>
2024-04-24 19:57:06 +0630

    Express redirect and 404

--- node_server/app.js
+++ node_server/app.js
@@ -5,8 +5,20 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
+//rendering html file
 app.get("/about", function (req, res) {
-  res.sendFile("./views/about.html", { root: __dirname }); //rendering html file
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+//redirect url
+app.get("/about-us", function (req, res) {
+  res.redirect("/about");
+});
+
+//404page - node reaches this line only if above routes are not match
+app.use((req, res) => {
+  res.statusCode = 404;
+  res.sendFile("./views/404.html", { root: __dirname });
 });
 
 app.listen(3000, () => {
