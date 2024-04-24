751042d1f79becbf2198b14ae8d1074a380d2344
KennethMinn <minthukha2472005@gmail.com>
2024-04-24 20:26:04 +0630

    Ejs view engine

--- node_server/app.js
+++ node_server/app.js
@@ -1,24 +1,27 @@
-const express = require("express");
+const express = require("express"); //npm install express
 const app = express();
 
+//view engine - npm install ejs
+app.set("views", "./views"); //folder
+app.set("view engine", "ejs"); //file
+
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.render("home"); //no need extension(ejs) of the file
 });
 
-//rendering html file
+//rendering file
 app.get("/about", function (req, res) {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
 });
 
 //redirect url
 app.get("/about-us", function (req, res) {
-  res.redirect("/about");
+  res.render("about");
 });
 
 //404page - node reaches this line only if above routes are not match
 app.use((req, res) => {
-  res.statusCode = 404;
-  res.sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404"); //short hand of two lines
 });
 
 app.listen(3000, () => {
