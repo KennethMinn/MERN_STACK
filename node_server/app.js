b933a72a59e8c127de1d80b68ce95a33a8c757d8
KennethMinn <minthukha2472005@gmail.com>
2024-04-24 19:32:03 +0630

    Express basic routing

---
+++ node_server/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.get("/about", function (req, res) {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+app.listen(3000, () => {
+  console.log("app is listening port 3000");
+});
