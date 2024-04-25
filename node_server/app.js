c97776dcf8286f85175b7931f8458d7e64ebd667
KennethMinn <minthukha2472005@gmail.com>
2024-04-25 20:46:43 +0630

    Connectiong to mongodb with altas

--- node_server/app.js
+++ node_server/app.js
@@ -1,7 +1,26 @@
 const express = require("express"); //npm install express
 let morgan = require("morgan"); //npm install morgan - middleware package
 
+const mongoose = require("mongoose"); //npm install mongoose
 const app = express();
+
+//db url
+let mongoUrl =
+  "mongodb+srv://mtk:mtk123@cluster0.yilh14y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+//connecting to database
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log("connect to db");
+    //server should only run after connecting to database
+    app.listen(3000, () => {
+      console.log("app is listening port 3000");
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 //view engine - npm install ejs
 app.set("views", "./views"); //folder
@@ -27,8 +46,4 @@ app.get("/about-us", function (req, res) {
 //404page - node reaches this line only if above routes are not match or function didn't respond
 app.use((req, res) => {
   res.status(404).render("404"); //short hand of two lines
-});
-
-app.listen(3000, () => {
-  console.log("app is listening port 3000");
 });
