1eb20913bd8b2efb03a565723e2e4db6bac8375f
KennethMinn <minthukha2472005@gmail.com>
2024-04-27 21:14:01 +0630

    Controller

---
+++ node_server/controllers/BlogController.js
@@ -0,0 +1,30 @@
+const Blog = require("../models/Blog");
+
+const BlogController = {
+  index: async function (req, res) {
+    const blogs = await Blog.find().sort({ createdAt: -1 });
+    res.json(blogs);
+  },
+  show: async (req, res) => {
+    console.log(req.params); //{id : "662bc6642ccd5b138a42484c"}
+    const blog = await Blog.findById(req.params.id);
+    res.json(blog);
+  },
+  store: async (_req, res) => {
+    let blog = new Blog({
+      title: "blog title 1",
+      intro: "blog intro 1",
+      body: "blog body 1",
+    });
+
+    //save in atlas
+    await blog.save(); //below lines will wait this line
+    res.send("blog created");
+  },
+  destory: async (req, res) => {
+    await Blog.findByIdAndDelete(req.params.id);
+    res.redirect("/blogs");
+  },
+};
+
+module.exports = BlogController;
