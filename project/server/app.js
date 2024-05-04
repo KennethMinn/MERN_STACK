87efd013f131ccb9fbd286e847006d6fce955bbf
KennethMinn <minthukha2472005@gmail.com>
2024-05-04 14:24:34 +0630

    Project setup

--- project/server/app.js
+++ project/server/app.js
@@ -1,3 +1,10 @@
 const express = require("express");
+const morgan = require("morgan"); //npm i morgan
+require("dotenv").config(); //npm i dotenv
 
 const app = express();
+app.use(morgan("dev"));
+
+app.listen(process.env.PORT, () => {
+  console.log("running");
+});
